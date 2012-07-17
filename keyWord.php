a4f4cc9179c93bb506ee3982837885b5b7f8f6fa
daniel ma <danielmayx@gmail.com>
2012-07-17 12:09:43 +0800

    keyword commit

--- keyWord.php
+++ keyWord.php
@@ -1,5 +1,7 @@
-<?php
-   include_once("session.php");
+  <?php
+  include_once("session.php");
+ 
+  
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -9,7 +11,9 @@
 	<title>布八哥消费者关系营销系统</title>
 	
     <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=297024590" type="text/javascript" charset="utf-8"></script>
-	<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
+	<!--<script src="ajax.js"></script> -->
+
+    <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
 	<!--[if lt IE 9]>
 	<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -19,10 +23,12 @@
 </head>
 
 <body>
+
 <?php
    
 	//include top and left bar of this page   
 	include_once ("top.php");
+	$merchantID = 1;
 	
 ?>
 <section id="secondary_bar">
@@ -38,11 +44,98 @@
  <?php include_once ("leftNavi.php"); ?>
 
 <section id="main" class="column">
-		<form class="quick_search" method="get">
-			<input type="text" name="keyWord" value="输入您要搜索的关键词组合" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
-		</form>
+		
+    <article class="module width_3_quarter">
+     <header>
+             <form method="get">
+           
+             <input type="text" id="ipsearch" name="q"  value="输入您要搜索的关键词组合" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;"> 
+           
+             <input type="submit"  value="添加关键词"  />
+             <input type="submit"   value="查询"  />
+             </form>
+     </header>
+     
+    	<table class="tablesorter">
+        	<header>
+            	<tr>
+                	<th width="10%">自动评论</th>
+                	<th>关键词</th>
+                    <th>评论内容</th>
+                    <th>统计</th>
+                    <th>操作</th>
+                </tr>
+                
+               
+                
+                
+                <?php 
+				// get data from db
+				  include_once("config.php");
+				
+
+				//connect to db
+				try {
+					$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+					$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+				}
+				catch(Exception $e){
+					die(var_dump($e));
+				}
+				
+			
+						
+						if( ! $conn )
+						{
+							die('注册出现问题，请稍后再试'.mysql_error() );
+						}
+						else
+						{
+							$sql = " select * from keyword where merchantID =$merchantID " ;
+							$result = $conn->query($sql);
+							$row = $result->fetchAll();
+							if( count($row) > 0 )
+							{
+								foreach( $row as $onerow )
+								{
+									// dynamic add table columns
+									?>
+									<tr>
+                                        <td align="center"><input name="checked" type="checkbox"   <?php if($onerow['autoTrack']) echo ("checked=\"checked\"");?> /></td>
+                                    	<td align="center"><a href="?q=<?=$onerow['words']?>"  >
+										<?=$onerow['words'] ?> </a>
+                                        </td>
+                                    	<td align="center"><?=$onerow['contentID'] ?></td>
+                                        <td align="center"></td>
+                                        <td align="center"><input type="submit" value="delete" name="del<?=$onerow['idkeyWord']?>" /></td>
+                                    </tr> 
+                                   
+                                    <?php
+									
+								}
+							}
+						}
+				
+				
+				?>
     
-		<wb:livestream skin="silver" publish="n" appkey="297024590" topic=" <?php echo urlencode($_GET["keyWord"])."|".urlencode($_GET["keyWord"]); ?>" width="auto" height="700" ></wb:livestream>
+            </header>
+        </table>
+    </article>
+
+
+
+                
+                
+                
+                
+                
+            
+    <div class="clear"></div>
+    
+    <article class="module width_full">
+		<wb:livestream skin="silver" publish="n" appkey="297024590" id="wbkeyWord" topic="<?=urlencode($_GET["q"])."|".urlencode($_GET["q"])?>" width="auto" height="700" ></wb:livestream>
+	</article>
 </section>
 
 </body>
